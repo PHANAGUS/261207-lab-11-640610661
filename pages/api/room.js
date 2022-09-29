f3649eb9ae534e4560b812189fc58ee14e3734cb
PHANAGUS <phanarin03072002@gmail.com>
2022-09-30 01:47:13 +0700

    lab-11

--- pages/api/room.js
+++ pages/api/room.js
@@ -2,14 +2,24 @@ import { checkToken } from "../../backendLibs/checkToken";
 import { readChatRoomsDB } from "../../backendLibs/dbLib";
 
 export default function roomRoute(req, res) {
-  const user = checkToken(req);
-  if (!user) {
-    return res.status(401).json({
-      ok: false,
-      message: "Yon don't permission to access this api",
-    });
-  }
-  const chatrooms = readChatRoomsDB();
+  if (req.method === "GET") {
+    // check token
 
-  //create room data and return response
+    const user = checkToken(req);
+    if (!user) {
+      return res.status(401).json({
+        ok: false,
+        message: "Yon don't permission to access this api",
+      });
+    }
+
+    //create room data and return response
+
+    const chatrooms = readChatRoomsDB();
+    chatrooms.map((x) => {
+      delete x.messages;
+    });
+
+    return res.json({ ok: true, rooms: chatrooms });
+  }
 }
--- pages/api/room/[roomId]/message/[messageId].js
+++ pages/api/room/[roomId]/message/[messageId].js
@@ -5,18 +5,51 @@ import {
 } from "../../../../../backendLibs/dbLib";
 
 export default function roomIdMessageIdRoute(req, res) {
-  //get ids from url
-  const roomId = req.query.roomId;
-  const messageId = req.query.messageId;
+  if (req.method === "DELETE") {
+    //get ids from url
+    const roomId = req.query.roomId;
+    const messageId = req.query.messageId;
 
-  //check token
+    //check token
+    const user = checkToken(req);
+    if (!user) {
+      return res.status(401).json({
+        ok: false,
+        message: "Yon don't permission to access this api",
+      });
+    }
 
-  const rooms = readChatRoomsDB();
+    const rooms = readChatRoomsDB();
 
-  //check if roomId exist
+    //check if roomId exist
+    const roomIdx = rooms.findIndex((x) => x.roomId === roomId);
+    if (roomIdx === -1)
+      return res.status(404).json({ ok: false, message: "Invalid room id" });
 
-  //check if messageId exist
+    //check if messageId exist
+    const messageIdx = rooms[roomIdx].messages.findIndex(
+      (x) => x.messageId === messageId
+    );
+    if (messageIdx === -1)
+      return res.status(404).json({ ok: false, message: "Invalid message id" });
 
-  //check if token owner is admin, they can delete any message
-  //or if token owner is normal user, they can only delete their own message!
+    //check if token owner is admin, they can delete any message
+    if (user.isAdmin) {
+      rooms[roomIdx].messages.splice(messageIdx, 1);
+      writeChatRoomsDB(rooms);
+      return res.json({ ok: true });
+    }
+    //or if token owner is normal user, they can only delete their own message!
+    else {
+      if (rooms[roomIdx].messages[messageIdx].username === user.username) {
+        rooms[roomIdx].messages.splice(messageIdx, 1);
+        writeChatRoomsDB(rooms);
+        return res.json({ ok: true });
+      }
+      return res.status(403).json({
+        ok: false,
+        message: "You do not have permission to access this data",
+      });
+    }
+  }
 }
